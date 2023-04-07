72d40f85ef6579a04e61b2238ef780d24ab18dce
Júlio Soares <juliocsoaresa1@gmail.com>
2023-04-07 12:17:10 -0300

    feat: ifs/switch
    Entendendo como funciona os ifs e o switch no Golang

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -20,10 +20,31 @@ func main() {
 	fmt.Println("\n1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+	fmt.Println("Escolha uma opção: ")
 
 	var comando int
 
 	fmt.Scan(&comando)
-	fmt.Println("O comando escolhido foi: ", comando)
+
+	// if comando == 1 {
+	// 	fmt.Println("Monitorando...")
+	// } else if comando == 2 {
+	// 	fmt.Println("Exibindo Logs...")
+	// } else if comando == 0 {
+	// 	fmt.Println("Saindo do Programa...")
+	// } else {
+	// 	fmt.Println("Comando inválido.")
+	// }
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do Programa...")
+	default:
+		fmt.Println("Comando inválido.")
+	}
 
 }
