9c41afb866be2408780429fbc4fc65b8cc84f06b
Júlio Soares <juliocsoaresa1@gmail.com>
2023-04-15 17:29:46 -0300

    feat: Adicionando a função de criar novos produtos

--- src/go-web/controllers/produtos.go
+++ src/go-web/controllers/produtos.go
@@ -3,18 +3,38 @@ package controllers
 import (
 	"go-web/models"
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	//produtos := []Produto{
-	//	{Nome: "Camiseta", Descricao: "Camiseta branca", Preco: 10.00, Quantidade: 10},
-	//	{"Calça", "Calça jeans", 20.00, 20},
-	//	{"Meia", "Meia branca", 5.00, 50},
-	//	{"Sapato", "Sapato preto", 250.00, 5},
-	//}
 	todosOsProdutos := models.BuscaTodosOsProdutos()
-	temp.ExecuteTemplate(w, "index", todosOsProdutos)
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertidoParaFloat, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro ao converter o preço para float", err)
+		}
+		quantidadeConvertidaParaInt, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro ao converter a quantidade para int", err)
+		}
+		models.CriaNovoProduto(nome, descricao, precoConvertidoParaFloat, quantidadeConvertidaParaInt)
+		http.Redirect(w, r, "/", 301)
+	}
 }
