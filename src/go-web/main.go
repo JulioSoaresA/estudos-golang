d5b8deb43944c6444c98a5144642a548c607acff
Júlio Soares <juliocsoaresa1@gmail.com>
2023-04-15 00:08:07 -0300

    feat: conexão com o banco de dados

--- src/go-web/main.go
+++ src/go-web/main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/lib/pq"
 	"net/http"
 	"text/template"
 )
 
+func conectaBD() *sql.DB {
+	conexao := "user=postgres dbname=web_golang_loja password=postgres host=localhost sslmode=disable"
+	db, err := sql.Open("postgres", conexao)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -20,12 +32,42 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Camiseta branca", Preco: 10.00, Quantidade: 10},
-		{"Calça", "Calça jeans", 20.00, 20},
-		{"Meia", "Meia branca", 5.00, 50},
-		{"Sapato", "Sapato preto", 250.00, 5},
+	//produtos := []Produto{
+	//	{Nome: "Camiseta", Descricao: "Camiseta branca", Preco: 10.00, Quantidade: 10},
+	//	{"Calça", "Calça jeans", 20.00, 20},
+	//	{"Meia", "Meia branca", 5.00, 50},
+	//	{"Sapato", "Sapato preto", 250.00, 5},
+	//}
+	db := conectaBD()
+
+	selectAll, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectAll.Next() {
+		var id int
+		var nome, descricao string
+		var preco float64
+		var quantidade int
+
+		err = selectAll.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Id = id
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
 	}
 
 	temp.ExecuteTemplate(w, "index", produtos)
+	defer db.Close()
 }
